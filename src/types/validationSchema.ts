c51ee1a4879ee7817b0bba61a1d34e4d886d4afe
Felipe Paulino <32152196+FelipePaulino@users.noreply.github.com>
2024-09-24 04:26:26 -0300

    Barraca de fruta

--- src/types/validationSchema.ts
+++ src/types/validationSchema.ts
@@ -1,14 +1,12 @@
 "use client";
 import { z } from "zod";
 
-// Validação do campo de pesquisa
 export const searchSchema = z
   .string()
   .refine((value) => /^[^\d]*$/.test(value), {
     message: "Números não são permitidos",
   });
 
-// Esquema de validação para o formulário de frutas
 export const fruitSchema = z.object({
   name: z.string().nonempty("O nome da fruta é obrigatório"),
   quantity: z.preprocess(
@@ -22,5 +20,4 @@ export const fruitSchema = z.object({
   url: z.string().url("A URL deve ser válida"),
 });
 
-// Geração do tipo TypeScript a partir do esquema Zod
 export type FruitFormInputs = z.infer<typeof fruitSchema>;
